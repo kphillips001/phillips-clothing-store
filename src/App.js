b756ba94aab2bd68846ae7f525a25806cc21f184
Kevin <knphillips01@gmail.com>
2021-02-19 13:41:05 -0600

    storing user data in app

--- src/App.js
+++ src/App.js
@@ -14,18 +14,29 @@ class App extends React.Component {
     super();
 
     this.state = {
-      currentUser: null
+      currentUser: null,
     };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      // this.setState({ currentUser: user });
-      createUserProfileDocument(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          }, () => {
+            console.log(this.state)
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
