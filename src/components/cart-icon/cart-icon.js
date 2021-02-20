bf82403eb08372cd898e3ea366d9b5bff462d857
Kevin <knphillips01@gmail.com>
2021-02-20 15:57:16 -0600

    added redux to cart

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -11,5 +14,8 @@ const CartIcon = ({ toggleCartHidden }) => (
   </div>
 );
 
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+})
 
-export default CartIcon
+export default connect(null, mapDispatchToProps)(CartIcon)
