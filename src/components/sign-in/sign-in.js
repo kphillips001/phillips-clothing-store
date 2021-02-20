8b667ee30c9b4456465c0d2031356690ac44996d
Kevin <knphillips01@gmail.com>
2021-02-19 19:14:26 -0600

    few changes added

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -60,7 +60,7 @@ class SignIn extends React.Component {
             required
           />
           <div className='buttons'>
-            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton type='button'> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               {' '}
               Sign In With Google{' '}
