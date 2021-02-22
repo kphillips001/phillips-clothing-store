a42143afee8964653a82070b19d5a5e410dcc30d
Kevin <knphillips01@gmail.com>
2021-02-21 18:26:11 -0600

    added reselect

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { createStructuredSelector } from 'reselect';
 import CustomButton from '../custom-button/custom-button';
 import CartItem from '../cart-item/cart-Item';
 import { selectCartItems } from '../../redux/cart/cart.selectors'; 
@@ -18,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  cartItems: selectCartItems(state)
-});
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
+})
 
 export default connect(mapStateToProps)(CartDropdown);
