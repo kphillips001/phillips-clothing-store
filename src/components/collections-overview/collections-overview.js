ecbc446800059947ab30ee620406f3044b18dc25
Kevin <knphillips01@gmail.com>
2021-02-23 13:38:56 -0600

    created Collection Overview Component

--- src/components/collections-overview/collections-overview.js
+++ src/components/collections-overview/collections-overview.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../collection-preview/collection-preview';
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
+import CollectionPreview  from '../collection-preview/collection-preview';
+
+import { selectCollections } from '../../redux/shop/shop.selectors'
 
 import './collections-overview.scss';
 
@@ -15,7 +17,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
+  collections: selectCollections,
 });
 
-export default connect(mapStateToProps)(CollectionsOverview);
+export default connect(mapStateToProps)(CollectionsOverview)
